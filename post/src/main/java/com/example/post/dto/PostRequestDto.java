32bc381475aeb5374bbc5c599fa9783ef4881ca5
powerhorang2 <tjrdbqls3@naver.com>
2021-08-13 12:59:44 +0900

    REST API - POST / @JsonProperty 어노테이션을 이용해서 이름이 다른 key 값을 연결시키는 방법 추가

--- post/src/main/java/com/example/post/dto/PostRequestDto.java
+++ post/src/main/java/com/example/post/dto/PostRequestDto.java
@@ -1,65 +1,98 @@
 package com.example.post.dto;
 
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public class PostRequestDto {
 
 	private String account;
 	private String email;
 	private String address;
 	private String password;
+	
+	@JsonProperty("phone_number")
+	private String phoneNumber; // phone_number
+
 	/**
 	 * @return the account
 	 */
 	public String getAccount() {
 		return account;
 	}
+
 	/**
 	 * @param account the account to set
 	 */
 	public void setAccount(String account) {
 		this.account = account;
 	}
+
 	/**
 	 * @return the email
 	 */
 	public String getEmail() {
 		return email;
 	}
+
 	/**
 	 * @param email the email to set
 	 */
 	public void setEmail(String email) {
 		this.email = email;
 	}
+
 	/**
 	 * @return the address
 	 */
 	public String getAddress() {
 		return address;
 	}
+
 	/**
 	 * @param address the address to set
 	 */
 	public void setAddress(String address) {
 		this.address = address;
 	}
+
 	/**
 	 * @return the password
 	 */
 	public String getPassword() {
 		return password;
 	}
+
 	/**
 	 * @param password the password to set
 	 */
 	public void setPassword(String password) {
 		this.password = password;
 	}
+
+	/**
+	 * @return the phoneNumber
+	 */
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+
+	/**
+	 * @param phoneNumber the phoneNumber to set
+	 */
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+
 	@Override
 	public String toString() {
 		return "PostRequestDto [account=" + account + ", email=" + email + ", address=" + address + ", password="
-				+ password + "]";
+				+ password + ", phoneNumber=" + phoneNumber + "]";
 	}
 	
-	
-	
+	@PostMapping("/post/dto")
+	public void post(@RequestBody PostRequestDto requestData) {
+		System.out.println(requestData);
+	}
 }
